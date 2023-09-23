978f1babbbb9e2b070262441eb097e185b64a7ca
ShujaShah <shuja.ashraf11@gmail.com>
2023-09-23 21:36:12 +0530

    added crud operations for customers

--- routes/customers.js
+++ routes/customers.js
@@ -11,6 +11,7 @@ const CustomerSchema = new mongoose.Schema({
     minlength: 5,
     maxlength: 50,
     lowercase: true,
+    unique: true,
   },
   name: {
     type: String,
@@ -46,6 +47,41 @@ router.post('/', async (req, res) => {
     phone: req.body.phone,
   });
   customer = await customer.save();
+  res.status(201).send(customer);
+});
+
+//Update Customer
+router.put('/:id', async (req, res) => {
+  const { error } = validateCustomer(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  let customer = await Customer.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      isGold: req.body.isGold,
+    },
+    { new: true }
+  );
+  if (!customer)
+    res.status(404).send('Customer with the given id not found...');
+  res.status(201).send(customer);
+});
+
+//Deleting a Customer
+router.delete('/:id', async (req, res) => {
+  let customer = await Customer.findByIdAndRemove(req.params.id);
+  if (!customer)
+    return res.status(404).send('Customer with given id not found');
+  res.status(201).send(customer);
+});
+
+// Getting a Single Customer
+router.get('/:id', async (req, res) => {
+  const customer = await Customer.findById(req.params.id);
+  if (!customer)
+    return res.status(404).send('Customer with Given id not found');
   res.status(201).send(customer);
 });
 
